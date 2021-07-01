8d5dd741aaaebae4d1b9335022ec70e627823d30
DDing77 <smg76262585@gmail.com>
2021-07-01 17:25:09 +0900

    1000

---
+++ 1000/src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a;
+        int b;
+        Scanner input = new Scanner(System.in);
+        a = input.nextInt();
+        b = input.nextInt();
+        System.out.println(a+b);
+    }
+}
