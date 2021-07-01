b6f8c0973a672c671eab8074cf6b4f23803190d6
DDing77 <smg76262585@gmail.com>
2021-07-01 17:29:36 +0900

    9498

---
+++ 9498/src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int score = input.nextInt();
+
+        if (score > 89) {
+            System.out.print("A");
+        } else if (score >= 80) {
+            System.out.print("B");
+        } else if (score >= 70) {
+            System.out.print("C");
+        } else if (score >= 60) {
+            System.out.print("D");
+        } else
+            System.out.print("F");
+    }
+}
