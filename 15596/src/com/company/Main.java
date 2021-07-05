7652c995db82b3e0846bf7f2dd588a8892e16e67
DDing77 <smg76262585@gmail.com>
2021-07-05 21:37:19 +0900

    15596

---
+++ 15596/src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    class Test {
+        long sum(int[] a) {
+            long sum = 0;
+            for (int i = 0; i < a.length; i++) sum += a[i];
+            return sum;
+        }
+    }
+}
