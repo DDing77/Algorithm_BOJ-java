df89e8e19f7d753a56bb9d00ae0b16f9daf8443d
DDing77 <smg76262585@gmail.com>
2021-07-17 05:50:02 +0900

    1463

---
+++ 1463/src/Main.java
@@ -0,0 +1,15 @@
+
+import java.io.*;
+
+public class Main { // BOJ_1463
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int X = Integer.parseInt(br.readLine());
+        System.out.println(operator(X, 0));
+    }
+
+    static int operator(int X, int count) {
+        if (X < 2) return count;
+        return Math.min(operator(X / 2, count + 1 + (X % 2)), operator(X / 3, count + 1 + (X % 3)));
+    }
+}
