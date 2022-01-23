793053e5483bf86b91b6019512e1b0e93e49d05d
DDing77 <smg76262585@gmail.com>
2022-01-23 22:44:48 +0900

    BOJ_15655_N과 M (6)

---
+++ 15655/src/Main.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static StringBuilder sb = new StringBuilder();
+    static int N, M;
+    static int[] arr;
+    static int[] res;
+    static boolean visit[];
+
+    static void BT(int depth, int start) {
+        if (depth == M) {
+            for (int i = 0; i < N; i++) {
+                sb.append(res[i]).append(" ");
+            }
+            sb.append('\n');
+            return;
+        }
+        for (int i = start; i < N; i++) {
+            if (!visit[i]) {
+                visit[i] = true;
+                res[depth] = arr[i];
+                BT(depth + 1, i + 1);
+                visit[i] = false;
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new int[N];
+        visit = new boolean[N];
+        res = new int[N];
+        Arrays.sort(arr);
+        BT(0, 0);
+        System.out.println(sb);
+    }
+}
