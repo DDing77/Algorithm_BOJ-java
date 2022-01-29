3e3b2f9c852c44192ecf231fdfec6fb858465db5
DDing77 <smg76262585@gmail.com>
2022-01-30 03:53:52 +0900

    BOJ_3176_도로 네트워크

--- 3176/src/Main.java
+++ 3176/src/Main.java
@@ -15,9 +15,11 @@ public class Main {
         }
     }
 
-    static int N, M, LogN, sum;
+    static int N, M, LogN;
+    static int min, max;
     static int[] depth;
-    static long[] distance;
+    static int[][] maxDistance;
+    static int[][] minDistance;
     static int[][] parent;
     static ArrayList<Info>[] map;
 
@@ -31,15 +33,16 @@ public class Main {
     static void bfs(int start) {
         Queue<Integer> que = new LinkedList<>();
         depth[start] = 1;
-        distance[start] = 0;
         que.add(start);
+
         while (!que.isEmpty()) {
             int now = que.poll();
             for (Info next : map[now]) {
                 if (depth[next.to] == 0) {
                     depth[next.to] = depth[now] + 1;
-                    distance[next.to] = distance[now] + next.w;
                     parent[0][next.to] = now;
+                    maxDistance[0][next.to] = next.w;
+                    minDistance[0][next.to] = next.w;
                     que.add(next.to);
                 }
             }
@@ -50,6 +53,8 @@ public class Main {
         for (int i = 1; i <= LogN; i++) {
             for (int j = 1; j <= N; j++) {
                 parent[i][j] = parent[i - 1][parent[i - 1][j]];
+                minDistance[i][j] = Math.min(minDistance[i-1][j], minDistance[i-1][parent[i-1][j]]);
+                maxDistance[i][j] = Math.max(maxDistance[i-1][j], maxDistance[i-1][parent[i-1][j]]);
             }
         }
     }
@@ -58,8 +63,13 @@ public class Main {
         if (depth[b] > depth[a]) {
             return getLca(b, a);
         }
+        min = 1000001;
+        max = 0;
+
         for (int i = 0; i <= LogN; i++) {
             if (((depth[a] - depth[b]) & (1 << i)) >= 1) {
+                min = Math.min(min, minDistance[i][a]);
+                max = Math.max(max, maxDistance[i][a]);
                 a = parent[i][a];
             }
         }
@@ -67,10 +77,14 @@ public class Main {
 
         for (int i = LogN; i >= 0; i--) {
             if (parent[i][a] != parent[i][b]) {
+                min = Math.min(min, Math.min(minDistance[i][a], minDistance[i][b]));
+                max = Math.max(max, Math.max(maxDistance[i][a], maxDistance[i][b]));
                 a = parent[i][a];
                 b = parent[i][b];
             }
         }
+        min = Math.min(min, Math.min(minDistance[0][a], minDistance[0][b]));
+        max = Math.max(max, Math.max(maxDistance[0][a], maxDistance[0][b]));
         return parent[0][a];
     }
 
@@ -80,10 +94,12 @@ public class Main {
         StringBuilder sb = new StringBuilder();
         N = Integer.parseInt(br.readLine());
         depth = new int[N + 1];
-        distance = new long[N + 1];
         map = new ArrayList[N + 1];
+
         getLogN();
         parent = new int[LogN + 1][N + 1];
+        maxDistance = new int[LogN+ 1][N + 1];
+        minDistance = new int[LogN+ 1][N + 1];
         for (int i = 0; i <= N; i++) {
             map[i] = new ArrayList<>();
         }
@@ -106,13 +122,9 @@ public class Main {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-
-            int lca = getLca(a, b);
-//            System.out.println("lca : " + lca);
-            long sum = distance[a] + distance[b] - 2 * distance[lca];
-            sb.append(sum).append('\n');
+            getLca(a,b);
+            sb.append(min).append(" ").append(max).append('\n');
         }
         System.out.print(sb);
-//        System.out.println(Arrays.toString(distance));
     }
 }
