59c35f4e689618164f5644f7f249dde93da0c3c2
DDing77 <smg76262585@gmail.com>
2022-02-22 03:40:30 +0900

    BOJ_11055_가장 큰 증가 부분 수열

--- 11055/src/Main.java
+++ 11055/src/Main.java
@@ -1,25 +1,33 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Deque;
-import java.util.LinkedList;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
     static int N;
+    static int[] arr;
+    static int[] dp;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
+        arr = new int[N+1];
+        dp  =new int[N+1];
 
-        Deque<Integer> dq = new LinkedList<>();
-        for(int i=1; i<=N; i++) {
-            dq.add(i);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i=1; i<=N; i++) arr[i] = Integer.parseInt(st.nextToken());
+
+        dp[1] = arr[1];
+        for(int i=2; i<=N; i++) {
+            dp[i] = arr[i];
+            for(int j=1; j<i; j++){
+                if(arr[i] > arr[j]) dp[i] = Math.max(dp[i], dp[j] + arr[i]);
+            }
         }
 
-        while(dq.size()>=2) {
-            dq.pollFirst();
-            int first = dq.poll();
-            dq.addLast(first);
-        }
-        System.out.println(dq.peek());
+        Arrays.sort(dp);
+        System.out.println(dp[N]);
     }
 }
+
