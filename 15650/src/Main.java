a06de3eacaae4c1c2abd188e670273963a9ba73b
DDing77 <smg76262585@gmail.com>
2022-03-05 09:40:56 +0900

    N과M(2)

--- 15650/src/Main.java
+++ 15650/src/Main.java
@@ -3,33 +3,68 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
-    static StringBuilder sb = new StringBuilder();
-    static int[] deps;
-    static int N, M;
+//public class Main {
+//    static StringBuilder sb = new StringBuilder();
+//    static int[] deps;
+//    static int N, M;
+//
+//    static void rec_func(int at, int k) {
+//        if (k == M) {
+//            for (int i = 0; i < M; i++) sb.append(deps[i]).append(' ');
+//            sb.append('\n');
+//            return;
+//        }
+//        for (int i = at; i <= N; i++) {
+//                deps[k] = i;
+//                rec_func(i+1, k+1);
+//        }
+//
+//    }
+//
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringTokenizer st = new StringTokenizer(br.readLine());
+//
+//        N = Integer.parseInt(st.nextToken());
+//        M = Integer.parseInt(st.nextToken());
+//        deps = new int[M];
+//        rec_func(1,0);
+//        System.out.println(sb.toString());
+//
+//    }
+//}
 
-    static void rec_func(int at, int k) {
-        if (k == M) {
-            for (int i = 0; i < M; i++) sb.append(deps[i]).append(' ');
+public class Main {
+    static int N, M;
+    static int[] depth;
+    static boolean[] isVisited;
+    static StringBuilder sb = new StringBuilder();
+
+    static void backTracking(int start, int x) {
+        if(x == M) {
+            for(int i=0; i<M; i++) {
+                sb.append(depth[i]).append(" ");
+            }
             sb.append('\n');
             return;
         }
-        for (int i = at; i <= N; i++) {
-                deps[k] = i;
-                rec_func(i+1, k+1);
+        for(int i= start; i<=N; i++) {
+            if(!isVisited[i]){
+                isVisited[i] = true;
+                depth[x] = i;
+                backTracking(i+1,x+1);
+                isVisited[i] =false;
+            }
         }
-
     }
-
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br  = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        deps = new int[M];
-        rec_func(1,0);
-        System.out.println(sb.toString());
-
+        depth = new int[M];
+        isVisited = new boolean[N+1];
+        backTracking(1,0);
+        System.out.print(sb);
     }
 }
