ab095707aaeb07ae0d255327a972dccce03fbbb3
DDing77 <smg76262585@gmail.com>
2022-03-22 22:23:02 +0900

    BOJ_3009_네 번째 점

--- 3009/src/Main.java
+++ 3009/src/Main.java
@@ -1,44 +1,27 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.*;
 
 public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] x = new int[3];
+        int[] y = new int[3];
+        int dx, dy;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        String[] coord_1 = br.readLine().split(" ");
-        String[] coord_2 = br.readLine().split(" ");
-        String[] coord_3 = br.readLine().split(" ");
-
-        String x;
-        String y;
-
-        if(coord_1[0].equals(coord_2[0])){
-            x = coord_3[0];
+        for(int i = 0; i < 3; i++) {
+            x[i] = sc.nextInt();
+            y[i] = sc.nextInt();
         }
 
-        else if (coord_1[0] == coord_3[0]) {
-            x = coord_2[0];
-        }
+        if(x[1] == x[2])
+            dx = x[0];
+        else
+            dx = (x[0] == x[1]) ? x[2] : x[1];
 
-        else {
-            x = coord_1[0];
-        }
+        if(y[1] == y[2])
+            dy = y[0];
+        else
+            dy = (y[0] == y[1]) ? y[2] : y[1];
 
-        if (coord_1[1] == coord_2[1]) {
-            y = coord_3[1];
-        }
-
-        else if (coord_1[1] == coord_3[1]) {
-            y = coord_2[1];
-        }
-
-        else {
-            y = coord_1[1];
-        }
-
-        System.out.println(x + " " + y);
+        System.out.println(dx + " " + dy);
     }
-
 }
