d4534d64eeb1ad3fe163416243684360b341c521
DDing77 <smg76262585@gmail.com>
2022-05-01 23:30:37 +0900

    BOJ_1107_리모컨

--- 1107/src/Main.java
+++ 1107/src/Main.java
@@ -24,21 +24,7 @@ public class Main {
         return len;
     }
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-
-        N = Integer.parseInt(br.readLine());
-        M = Integer.parseInt(br.readLine());
-
-
-        min = Math.abs(N - 100);
-
-        error = new boolean[10];
-
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < M; i++) error[Integer.parseInt(st.nextToken())] = true;
-
+    static void bruteForce() {
         for (int i = 0; i < 1000000; i++) {
             int len = possible(i);
 
@@ -48,6 +34,25 @@ public class Main {
                 if (min > len + plusCnt) min = len + plusCnt;
             }
         }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        N = Integer.parseInt(br.readLine());
+        M = Integer.parseInt(br.readLine());
+
+        min = Math.abs(N - 100);
+
+        error = new boolean[10];
+        if (M != 0) {
+            st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < M; i++) error[Integer.parseInt(st.nextToken())] = true;
+        }
+
+        bruteForce();
+
         System.out.println(min);
     }
 }
