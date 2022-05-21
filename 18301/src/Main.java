07eae51e3ceb4d33dc334653814cc78e07c7ef8b
DDing77 <smg76262585@gmail.com>
2022-05-21 22:35:36 +0900

    BOJ_18301_Rats

---
+++ 18301/src/Main.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n1 = Integer.parseInt(st.nextToken());
+        int n2 = Integer.parseInt(st.nextToken());
+        int n12 = Integer.parseInt(st.nextToken());
+
+        int res = (n1 + 1) * (n2 + 1) / (n12 + 1) - 1;
+        System.out.println(res);
+    }
+}
