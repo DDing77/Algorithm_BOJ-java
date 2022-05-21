42a54d858e7463bace5c931b2488b4df4bc2fd62
DDing77 <smg76262585@gmail.com>
2022-05-21 22:53:53 +0900

    BOJ_24086_身長 (Height)

---
+++ 24086/src/Main.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int X = Integer.parseInt(br.readLine());
+        int Y = Integer.parseInt(br.readLine());
+
+        System.out.println(Math.abs(X - Y));
+    }
+}
