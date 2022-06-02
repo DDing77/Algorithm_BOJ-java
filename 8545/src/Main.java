714c5666d53f75986af2ef3ac64f605f883c75a3
DDing77 <smg76262585@gmail.com>
2022-06-02 22:50:41 +0900

    BOJ_8545_Zadanie próbne

---
+++ 8545/src/Main.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(br.readLine());
+        sb.reverse();
+        System.out.println(sb);
+    }
+}
