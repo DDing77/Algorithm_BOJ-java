e5b7dd03a39889198409d73ede9575ecb924e601
DDing77 <smg76262585@gmail.com>
2022-06-10 11:32:24 +0900

    BOJ_4101_크냐?

---
+++ 4101/src/Main.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        while (true) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            if (a == 0 && b == 0) break;
+            if (a > b) sb.append("Yes").append('\n');
+            else sb.append("No").append('\n');
+        }
+
+        System.out.print(sb);
+    }
+}
