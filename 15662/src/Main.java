863720a5af41e18957841f9ab752770bf6c6d55d
DDing77 <smg76262585@gmail.com>
2022-06-30 20:24:57 +0900

    BOJ_15662_톱니바퀴 (2)

---
+++ 15662/src/Main.java
@@ -0,0 +1,92 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int T, K;
+    static int res;
+    static int[][] gear;
+    static boolean[] isRotate;
+
+    static int getScore() {
+        int sum = 0;
+
+        for (int i = 0; i < T; i++) {
+            if (gear[i][0] == 1) sum++;
+        }
+
+        return sum;
+    }
+
+    static void changeSN(int gearNum, int dir) {
+        int temp = gear[gearNum][0];
+
+        if (dir == -1) { // 반시계 방향 회전
+            gear[gearNum][0] = gear[gearNum][1];
+            gear[gearNum][1] = gear[gearNum][2];
+            gear[gearNum][2] = gear[gearNum][3];
+            gear[gearNum][3] = gear[gearNum][4];
+            gear[gearNum][4] = gear[gearNum][5];
+            gear[gearNum][5] = gear[gearNum][6];
+            gear[gearNum][6] = gear[gearNum][7];
+            gear[gearNum][7] = temp;
+        } else { // 시계 방향 회전
+            gear[gearNum][0] = gear[gearNum][7];
+            gear[gearNum][7] = gear[gearNum][6];
+            gear[gearNum][6] = gear[gearNum][5];
+            gear[gearNum][5] = gear[gearNum][4];
+            gear[gearNum][4] = gear[gearNum][3];
+            gear[gearNum][3] = gear[gearNum][2];
+            gear[gearNum][2] = gear[gearNum][1];
+            gear[gearNum][1] = temp;
+        }
+    }
+
+    static void rotate(int target, int dir) {
+        // 톱니바퀴 0 ~ 3
+        // 오른쪽 톱니 2, 왼쪽 톱니 6
+        if (target < 0 || target > T) return;
+
+        isRotate[target] = true;
+
+        // 왼쪽이랑 다름
+        if (target - 1 >= 0 && gear[target - 1][2] != gear[target][6]) {
+            if (!isRotate[target - 1]) rotate(target - 1, -dir);
+        }
+        // 오른쪽이랑 다름
+        if (target + 1 < T && gear[target][2] != gear[target + 1][6]) {
+            if (!isRotate[target + 1]) rotate(target + 1, -dir);
+        }
+        // 회전
+        changeSN(target, dir);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        T = Integer.parseInt(br.readLine());
+        gear = new int[T][8];
+
+        for (int i = 0; i < T; i++) {
+            String input = br.readLine();
+            for (int j = 0; j < 8; j++) {
+                gear[i][j] = Integer.parseInt(String.valueOf(input.charAt(j)));
+            }
+        }
+
+        K = Integer.parseInt(br.readLine());
+        for (int i = 0; i < K; i++) {
+            st = new StringTokenizer(br.readLine());
+            int gearNumber = Integer.parseInt(st.nextToken());
+            int dir = Integer.parseInt(st.nextToken());
+            isRotate = new boolean[T];
+            rotate(gearNumber - 1, dir);
+        }
+
+        res = getScore();
+        System.out.println(res);
+    }
+}
