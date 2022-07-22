88d622323b13a88ffffb2d76ac565e2c1f3f029c
DDing77 <devseo2021@gmail.com>
2022-07-22 19:35:38 +0900

    BOJ_2725_보이는 점의 개수

--- 2725/src/Main.java
+++ 2725/src/Main.java
@@ -3,12 +3,36 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-    static int T;
+    static int C;
+    static int[] dp;
+
+    static int gcd(int a, int b) {
+        if (b == 0) return a;
+        return gcd(b, a % b);
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        for (int t = 0; t < T; t++) {
-            int N = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
 
+        dp = new int[1001];
+        dp[0] = 0;
+        dp[1] = 3;
+
+        for (int i = 2; i <= 1000; i++) {
+            int cnt = 0;
+            for (int j = 1; j < i; j++) {
+                if (gcd(i, j) == 1) cnt++;
+            }
+            dp[i] = dp[i - 1] + cnt * 2;
         }
+
+        C = Integer.parseInt(br.readLine());
+
+        for (int c = 0; c < C; c++) {
+            int N = Integer.parseInt(br.readLine());
+            sb.append(dp[N]).append('\n');
+        }
+        System.out.print(sb);
     }
 }
