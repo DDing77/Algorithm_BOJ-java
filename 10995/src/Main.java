c77c2522e89a32325f6ace2f54b928230c6e37d2
DDing77 <smg76262585@gmail.com>
2022-08-06 21:41:52 +0900

    BOJ_10995_별 찍기 - 20

---
+++ 10995/src/Main.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int N;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+
+        for(int i=0; i< N; i++) {
+            for(int j=0; j< 2 *N; j++) {
+                if( (i + j) % 2 == 0) System.out.print("*");
+                else System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
