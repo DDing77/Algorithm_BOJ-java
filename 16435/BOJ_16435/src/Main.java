e3054de360fcb5ef6a5ee75c00b9a2e9f0a50500
DDing77 <devseo2021@gmail.com>
2022-11-01 17:15:48 +0900

    BOJ_16435

---
+++ 16435/BOJ_16435/src/Main.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int N, L;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        L = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) arr[i] = Integer.parseInt(st.nextToken());
+
+        Arrays.sort(arr);
+
+        for (int i = 0; i < N; i++) {
+            if (L >= arr[i]) L++;
+            else break;
+        }
+
+        System.out.println(L);
+    }
+}
