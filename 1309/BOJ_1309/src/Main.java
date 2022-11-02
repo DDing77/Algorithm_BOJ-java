6173d85e378191226a4bc1ff61c8d4b7c76129b1
DDing77 <devseo2021@gmail.com>
2022-11-02 15:43:53 +0900

    BOJ_1309

---
+++ 1309/BOJ_1309/src/Main.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+
+    static int N;
+    static int[][] dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+        dp = new int[N][3];
+
+        Arrays.fill(dp[0], 1);
+
+        for (int i = 1; i < N; i++) {
+            dp[i][0] = (dp[i - 1][0] + dp[i - 1][1] + dp[i - 1][2]) % 9901;
+            dp[i][1] = (dp[i - 1][0] + dp[i - 1][2]) % 9901;
+            dp[i][2] = (dp[i - 1][0] + dp[i - 1][1]) % 9901;
+        }
+
+        System.out.println((dp[N - 1][0] + dp[N - 1][1] + dp[N - 1][2]) % 9901);
+    }
+}
