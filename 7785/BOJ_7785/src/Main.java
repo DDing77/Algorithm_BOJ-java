f6e3bf2ee980caf1f7733de73261e62a643cc55f
DDing77 <devseo2021@gmail.com>
2022-11-03 15:10:11 +0900

    BOJ_7785

---
+++ 7785/BOJ_7785/src/Main.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static HashSet<String> set;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        set = new HashSet<>();
+
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 0; i < n; i++) {
+            String[] input = br.readLine().split(" ");
+            String name = input[0];
+            String log = input[1];
+
+            if (log.equals("enter"))
+                set.add(name);
+            if (log.equals("leave")) {
+                if (set.contains(name))
+                    set.remove(name);
+            }
+        }
+
+        List<String> list = new ArrayList<>();
+        Iterator<String> it = set.iterator();
+
+        while (it.hasNext())
+            list.add(it.next());
+
+        Collections.sort(list, Collections.reverseOrder());
+
+        for (String name : list) sb.append(name).append('\n');
+
+        System.out.print(sb);
+
+    }
+
+}
