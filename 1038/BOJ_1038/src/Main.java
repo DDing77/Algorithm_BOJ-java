e53b213392a316e58ac9dfef4ca948c2b551ee14
DDing77 <devseo2021@gmail.com>
2022-11-23 20:02:58 +0900

    BOJ_1038

--- 1038/BOJ_1038/src/Main.java
+++ 1038/BOJ_1038/src/Main.java
@@ -2,23 +2,19 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
-import java.util.List;
 
 public class Main {
 
     static int N;
-    static List<Integer> desc;
+    static ArrayList<Long> desc;
 
-    private static void bt(int num, int idx) {
+    static void bt(long num, int idx) {
         if (idx > 10) return;
 
         desc.add(num);
 
-        for (int i = 0; i < 10; i++) {
-            if (num % 10 > i) bt(num * 10 + i, idx + 1);
-        }
+        for (int i = 0; i < num % 10; i++) bt((num * 10) + i, idx + 1);
 
         return;
     }
