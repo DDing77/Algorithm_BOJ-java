1b7f6c3741e661b5bd670386a02c064d52128500
DDing77 <devseo2021@gmail.com>
2023-01-03 19:59:46 +0900

    BOJ_3049

---
+++ 3049/BOJ_3049/src/Main.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    static int N;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+
+        System.out.println(N * (N - 1) * (N - 2) * (N - 3) / 24);
+    }
+}
