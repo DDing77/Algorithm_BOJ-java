6cab27ffaeb79aacb3f1e7dffecc667f8c9e0395
DDing77 <devseo2021@gmail.com>
2023-03-08 23:25:54 +0900

    BOJ_25305

---
+++ 25305/BOj_25305/src/Main.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    private int N, k;
+    private int[] scores;
+
+    public void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+
+        scores = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            scores[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(scores);
+
+        System.out.println(scores[N - k]);
+    }
+
+    public static void main(String[] args) throws IOException {
+        new Main().solution();
+    }
+}
