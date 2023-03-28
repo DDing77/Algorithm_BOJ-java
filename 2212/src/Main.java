638f430c31df4864c36f73a2997a4ffd658e7630
DDing77 <devseo2021@gmail.com>
2023-03-28 12:16:49 +0900

    BOJ_2212

--- 2212/src/Main.java
+++ 2212/src/Main.java
@@ -1,11 +1,46 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int N, K;
+
+    private int N, K;
+    private int[] sensors;
+    private int[] diff;
+    private int res;
+
+    public void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        N = Integer.parseInt(br.readLine());
+        K = Integer.parseInt(br.readLine());
+        sensors = new int[N];
+        diff = new int[N - 1];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            sensors[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(sensors);
+
+        for (int i = 1; i < N; i++) {
+            diff[i - 1] = sensors[i] - sensors[i - 1];
+        }
+
+        Arrays.sort(diff);
+
+        res = 0;
+        for (int i = 0; i < N - K; i++) {
+            res += diff[i];
+        }
+
+        System.out.println(res);
+    }
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        new Main().solution();
     }
 }
