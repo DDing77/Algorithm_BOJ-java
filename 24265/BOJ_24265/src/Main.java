c3a3cf2aa5cf447a825d62b0b11b8b529d72b2bb
DDing77 <devseo2021@gmail.com>
2023-05-29 08:16:22 +0900

    BOJ_24265

---
+++ 24265/BOJ_24265/src/Main.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    static long N;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Long.parseLong(br.readLine());
+
+        System.out.println(N * (N - 1) / 2);
+        System.out.println(2);
+    }
+}
