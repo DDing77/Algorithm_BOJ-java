48d837b39dc5ed8cf38c2e06bf1f362bd378d0c0
DDing77 <devseo2021@gmail.com>
2023-06-25 09:09:47 +0900

    BOJ_1002

--- 1000~2000/1002/src/Main.java
+++ 1000~2000/1002/src/Main.java
@@ -4,37 +4,83 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int getDot(int x1, int y1, int r1, int x2, int y2, int r2) {
-        int distance_pow = (int)(Math.pow(x2- x1,2) + Math.pow(y2-y1, 2)); // 중점과의 거리
 
-        // 중심좌표가 같음 => 무한
-        if(x1 == x2 && y1 == y2 && r1 == r2) return -1;
-        // 중점거리가 두 원의 반지름보다 길 떄
-        else if( distance_pow > Math.pow(r1+r2,2)) return 0;
-        else if(distance_pow < Math.pow(r2 - r1, 2)) return 0;
+    static int T;
+    static Circle[] circles;
 
-        // 내접
-        else if(distance_pow == Math.pow(r2 -r1,2)) return 1;
-        else if(distance_pow == Math.pow(r2 +r1,2)) return 1;
-        else return 2;
+    private static int getResult(Circle c1, Circle c2) {
+        int xDistance = (c1.x >= c2.x) ? c1.x - c2.x : c2.x - c1.x;
+        int yDistance = (c1.y >= c2.y) ? c1.y - c2.y : c2.y - c1.y;
+
+        if (xDistance == 0 && yDistance == 0) {
+            if (c1.r == c2.r && c1.r == 0) {
+                return 1;
+            } else if (c1.r == c2.r) {
+                return -1;
+            } else {
+                return 0;
+            }
+        }
+
+        double distance;
+        if (xDistance == 0) {
+            distance = yDistance;
+        } else if (yDistance == 0) {
+            distance = xDistance;
+        } else {
+            distance = Math.sqrt(Math.pow(xDistance, 2) + Math.pow(yDistance, 2));
+        }
+
+        if (distance == c1.r + c2.r) {
+            return 1;
+        } else if (distance > c1.r + c2.r) {
+            return 0;
+        } else {
+            if (c1.r == c2.r) {
+                return 2;
+            }
+
+            int bigger = Math.max(c1.r, c2.r);
+            int smaller = Math.min(c1.r, c2.r);
+
+            if (bigger - distance > smaller) {
+                return 0;
+            } else if (bigger - distance == smaller) {
+                return 1;
+            } else {
+                return 2;
+            }
+        }
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
 
-        int T = Integer.parseInt(br.readLine());
+        T = Integer.parseInt(br.readLine());
+        while (T-- > 0) {
+            circles = new Circle[2];
 
-        while(T-- > 0) {
-            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-            int x1 = Integer.parseInt(st.nextToken());
-            int y1 = Integer.parseInt(st.nextToken());
-            int r1 = Integer.parseInt(st.nextToken());
+            st = new StringTokenizer(br.readLine());
+            circles[0] = new Circle(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+            circles[1] = new Circle(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
 
-            int x2 = Integer.parseInt(st.nextToken());
-            int y2 = Integer.parseInt(st.nextToken());
-            int r2 = Integer.parseInt(st.nextToken());
+            sb.append(getResult(circles[0], circles[1])).append('\n');
+        }
 
-            System.out.println(getDot(x1, y1, r1, x2, y2, r2));
+        System.out.println(sb);
+    }
+
+    static class Circle {
+        int x;
+        int y;
+        int r;
+
+        public Circle(int x, int y, int r) {
+            this.x = x;
+            this.y = y;
+            this.r = r;
         }
     }
 }
