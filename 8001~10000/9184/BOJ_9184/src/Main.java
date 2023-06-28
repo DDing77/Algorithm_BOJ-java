878f7b68e7a855d406ccdd20eb90a3f5b851ec3e
DDing77 <devseo2021@gmail.com>
2023-06-28 20:01:01 +0900

    BOJ_9184

---
+++ 8001~10000/9184/BOJ_9184/src/Main.java
@@ -0,0 +1,60 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int a;
+    static int b;
+    static int c;
+    static int[][][] dp;
+
+    public static int w(int a, int b, int c) {
+
+        if (a <= 0 || b <= 0 || c <= 0) {
+            return 1;
+        }
+
+        if (a > 20 || b > 20 || c > 20) {
+            return w(20, 20, 20);
+        }
+
+        if (dp[a][b][c] != 0) {
+            return dp[a][b][c];
+        }
+
+        if (a < b && b < c) {
+            dp[a][b][c] = w(a, b, c - 1) + w(a, b - 1, c - 1) + w(a, b - 1, c);
+        }
+
+        return dp[a][b][c] = w(a - 1, b, c) + w(a - 1, b - 1, c) + w(a - 1, b, c - 1) - w(a - 1, b - 1, c - 1);
+    }
+
+    public static void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        dp = new int[21][21][21];
+        while (true) {
+            st = new StringTokenizer(br.readLine());
+            a = Integer.parseInt(st.nextToken());
+            b = Integer.parseInt(st.nextToken());
+            c = Integer.parseInt(st.nextToken());
+
+            if (a == -1 && b == -1 && c == -1) {
+                break;
+            }
+
+
+            sb.append("w(" + a + ", " + b + ", " + c + ") = " + w(a, b, c)).append('\n');
+        }
+
+        System.out.print(sb);
+    }
+
+    public static void main(String[] args) throws IOException {
+        solution();
+    }
+}
