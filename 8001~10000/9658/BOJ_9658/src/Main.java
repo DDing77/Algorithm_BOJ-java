fe1138029e48700b09b22650811fc166eb5423a6
DDing77 <devseo2021@gmail.com>
2023-08-27 14:46:56 +0900

    BOJ_9658

---
+++ 8001~10000/9658/BOJ_9658/src/Main.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    int N;
+
+    public void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+
+        if (N % 7 == 1 || N % 7 == 3) {
+            System.out.println("CY");
+        } else {
+            System.out.println("SK");
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        new Main().solution();
+    }
+}
