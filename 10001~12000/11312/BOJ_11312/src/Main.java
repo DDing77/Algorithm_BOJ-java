81e025ad0ea9a73dbc432db75d71409ac9443166
DDing77 <devseo2021@gmail.com>
2023-11-17 16:38:01 +0900

    BOJ_11312

---
+++ 10001~12000/11312/BOJ_11312/src/Main.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    int T;
+    int A;
+    int B;
+
+    public void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        T = Integer.parseInt(br.readLine());
+        while (T-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            A = Integer.parseInt(st.nextToken());
+            B = Integer.parseInt(st.nextToken());
+
+            sb.append((long)(A / B) * (A / B)).append('\n');
+        }
+        System.out.print(sb);
+    }
+
+    public static void main(String[] args) throws IOException {
+        new Main().solution();
+    }
+}
