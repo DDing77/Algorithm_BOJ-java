75b80bb9c7b6810f697adb0eb5a690e55aa7f819
DDing77 <devseo2021@gmail.com>
2024-01-15 19:35:53 +0900

    BOJ_2372

---
+++ 2001~4000/2372/BOJ_2372/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Animal      Count").append("\n");
+        sb.append("-----------------").append("\n");
+        sb.append("Chickens      100").append("\n");
+        sb.append("Clydesdales     5").append("\n");
+        sb.append("Cows           40").append("\n");
+        sb.append("Goats          22").append("\n");
+        sb.append("Steers          2");
+
+        System.out.println(sb);
+    }
+}
