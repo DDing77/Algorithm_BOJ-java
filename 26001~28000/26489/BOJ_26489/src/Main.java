2155967f367a64cd0ea9f99a409005a78ea59b1c
DDing77 <devseo2021@gmail.com>
2024-01-27 23:21:43 +0900

    BOJ_26489

---
+++ 26001~28000/26489/BOJ_26489/src/Main.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static final StringBuilder sb = new StringBuilder();
+    int res;
+
+    private void solution() throws IOException {
+        String input;
+        while ((input = br.readLine()) != null) {
+            res++;
+        }
+
+        System.out.println(res);
+    }
+
+    public static void main(String[] args) throws IOException {
+        Main main = new Main();
+        main.solution();
+    }
+}
