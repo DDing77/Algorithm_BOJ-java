470a4c3fb3b5045729506b6bfeaf2bfbee09df6d
DDing77 <devseo2021@gmail.com>
2024-01-31 07:23:26 +0900

    BOJ_4806

---
+++ 4001~6000/4806/BOJ_4806/src/Main.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static final StringBuilder sb = new StringBuilder();
+
+    String input;
+    int res;
+
+    private void solution() throws IOException {
+
+        res = 0;
+        while((input = br.readLine()) != null) {
+            res++;
+        }
+
+        System.out.println(res);
+    }
+
+    public static void main(String[] args) throws IOException {
+        Main main = new Main();
+        main.solution();
+    }
+}
