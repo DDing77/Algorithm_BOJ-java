664504aabcf1e041ebaf51981c192aa117060cc6
DDing77 <devseo2021@gmail.com>
2024-02-09 17:15:24 +0900

    BOJ_16504

---
+++ 16001~18000/16504/BOJ_16504/src/Main.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static final StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+
+    int N;
+    long res;
+
+    private void solution() throws IOException {
+
+        N = Integer.parseInt(br.readLine());
+
+        res = 0L;
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                res += Integer.parseInt(st.nextToken());
+            }
+        }
+
+        System.out.println(res);
+    }
+
+    public static void main(String[] args) throws IOException {
+        Main main = new Main();
+        main.solution();
+    }
+}
