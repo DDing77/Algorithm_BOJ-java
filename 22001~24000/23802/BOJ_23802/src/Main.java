18e2f27e948fc3a1028b2943d06e3fa43dedaf08
DDing77 <devseo2021@gmail.com>
2024-02-12 14:52:44 +0900

    BOJ_23802

---
+++ 22001~24000/23802/BOJ_23802/src/Main.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static final StringBuilder sb = new StringBuilder();
+
+    int N;
+
+    private void solution() throws IOException {
+
+        N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N * 5; j++) {
+                sb.append("@");
+            }
+            sb.append("\n");
+        }
+
+        for (int i = 0; i < N * 4; i++) {
+            for (int j = 0; j < N; j++) {
+                sb.append("@");
+            }
+            sb.append("\n");
+        }
+
+        System.out.print(sb);
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        Main main = new Main();
+        main.solution();
+    }
+}
