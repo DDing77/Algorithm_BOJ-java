7fc684ada2bd74db88b372b631fcd744fc5ed054
DDing77 <devseo2021@gmail.com>
2024-02-18 16:52:12 +0900

    BOJ_15818

---
+++ 14001~16000/15818/BOj_15818/src/Main.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static final StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+
+    int N;
+    int M;
+    long res;
+
+    private void solution() throws IOException {
+
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        res = 1L;
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            res *= (Integer.parseInt(st.nextToken()) % M);
+            res %= M;
+        }
+
+        System.out.println(res);
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        Main main = new Main();
+        main.solution();
+    }
+}
