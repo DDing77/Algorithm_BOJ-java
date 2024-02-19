86f40f74d93a549f8c7460c2a69cf38cc6bf10d3
DDing77 <devseo2021@gmail.com>
2024-02-20 08:57:03 +0900

    BOJ_commit.sh commit-time: Tue Feb 20 08:57:03 KST 2024

---
+++ BOJ_commit.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+echo '===== BOJ_commit.sh start ====='
+
+rootDir=/home/dding/algorithm/Algorithm_BOJ-java
+cd $rootDir
+echo pwd = `pwd`
+
+# bring the latest
+git pull
+
+# get file's path to commit
+filePath=`git status -u -s | head -n 1`
+filePath=${filePath:3}
+echo filePath = $filePath
+
+# Exit if file does not exist
+if [ -z "$filePath" ]; then
+	echo "===== file does not exist ====="
+	echo "===== BOJ.commit.sh end ====="
+	exit
+fi
+
+# get dirPath
+dirPath=`dirname $filePath`
+echo dirPath = $dirPath
+
+# get file's name
+fileName=`basename $filePath`
+echo fileName = $fileName
+
+# Generate commit message
+commitMessageTitle=$fileName
+commitMessageBody="commit-time: `date`"
+commitMessage="$commitMessageTitle"$'\n'"$commitMessageBody"
+echo commitMessage = $commitMessage
+
+# git commit
+cd $dirPath
+git add $fileName
+git commit -m "$commitMessage"
+git push origin main
+
+echo "===== BOJ_commit.sh end ====="
