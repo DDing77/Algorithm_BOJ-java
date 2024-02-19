ae974c0d7c4a27dedde212618f5b14e153e71c1b
DDing77 <devseo2021@gmail.com>
2024-02-19 19:33:47 +0900

    BOJ_23811

---
+++ 22001~24000/23811/BOJ_23811/src/Main.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static final StringBuilder sb = new StringBuilder();
+
+    int N;
+
+    private void generateType1() {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N * 5; j++) {
+                sb.append("@");
+            }
+            sb.append("\n");
+        }
+    }
+
+    private void generateType2() {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                sb.append("@");
+            }
+            sb.append("\n");
+        }
+    }
+
+    private void solution() throws IOException {
+
+        N = Integer.parseInt(br.readLine());
+
+        generateType1();
+        generateType2();
+        generateType1();
+        generateType2();
+        generateType1();
+
+        System.out.print(sb);
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        Main main = new Main();
+        main.solution();
+    }
+}
