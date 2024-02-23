aac721b3ee1c97f8a1a4acc2e22022a6e532725a
DDing77 <devseo2021@gmail.com>
2024-02-24 06:05:02 +0900

    BOJ_23810 commit-time: Sat Feb 24 06:05:02 KST 2024

---
+++ 22001~24000/23810/BOJ_23810/src/Main.java
@@ -0,0 +1,59 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static final StringBuilder sb = new StringBuilder();
+
+    int N;
+
+    private void generateType1() {
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < 5 * N; j++) {
+                sb.append("@");
+            }
+            sb.append("\n");
+        }
+    }
+
+    private void generateType2() {
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                sb.append("@");
+            }
+            sb.append("\n");
+        }
+    }
+
+    private void generateType3() {
+
+        for (int i = 0; i < 2 * N; i++) {
+            for (int j = 0; j < N; j++) {
+                sb.append("@");
+            }
+            sb.append("\n");
+        }
+    }
+
+    private void solution() throws IOException {
+
+        N = Integer.parseInt(br.readLine());
+
+        generateType1();
+        generateType2();
+        generateType1();
+        generateType3();
+
+        System.out.print(sb);
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        Main main = new Main();
+        main.solution();
+    }
+}
