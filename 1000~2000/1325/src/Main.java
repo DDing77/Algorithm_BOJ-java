8b132f32241b6dc5a0876a5584a56a6f2d6e0982
DDing77 <devseo2021@gmail.com>
2024-04-27 10:35:36 +0900

    BOJ_1325

--- 1000~2000/1325/src/Main.java
+++ 1000~2000/1325/src/Main.java
@@ -1,66 +1,95 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int N, M;
-    static boolean[] isVisited;
-    static int[] cnt;
-    static ArrayList<Integer>[] list;
-    static int max = Integer.MIN_VALUE;
 
-    static void BFS(int start) {
-        isVisited = new boolean[N + 1];
-        Queue<Integer> que = new LinkedList<>();
-        isVisited[start] = true;
+    public static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static final StringBuilder sb = new StringBuilder();
+    public static StringTokenizer st;
+
+    private int N;
+    private int M;
+    private ArrayList<Integer>[] edges;
+    private int[] count;
+    private boolean[][] isVisited;
+    private int max;
+
+    private void execBFS(int start) {
+
+        Queue<Integer> que = new ArrayDeque<>();
         que.add(start);
 
         while (!que.isEmpty()) {
-            int now = que.poll();
-            for (int next : list[now]) {
-                if (isVisited[next]) continue;
-                cnt[next]++;
-                que.add(next);
-                isVisited[next] = true;
+            int cur = que.poll();
+            if (isVisited[start][cur]) {
+                continue;
+            }
+            count[start]++;
+            isVisited[start][cur] = true;
 
-                if (max < cnt[next]) {
-                    max = cnt[next];
+            for (int next : edges[cur]) {
+                if (isVisited[start][next]) {
+                    continue;
+                }
+                if (next < start) {
+                    for (int i = 1; i <= N; i++) {
+                        if (isVisited[start][i]) {
+                            continue;
+                        }
+                        if (isVisited[next][i]) {
+                            count[start]++;
+                            isVisited[start][i] = true;
+                        }
+                    }
+                } else {
+                    que.add(next);
                 }
             }
         }
     }
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        StringTokenizer st = new StringTokenizer(br.readLine());
+    private void solution() throws IOException {
 
+        st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
-        cnt = new int[N + 1];
-        list = new ArrayList[N + 1];
-        for (int i = 0; i <= N; i++) list[i] = new ArrayList<>();
+        edges = new ArrayList[N + 1];
+        for (int i = 0; i <= N; i++) {
+            edges[i] = new ArrayList<>();
+        }
 
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
-
-            list[start].add(end);
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            edges[b].add(a);
         }
 
+        max = 0;
+        isVisited = new boolean[N + 1][N + 1];
+        count = new int[N + 1];
+        for (int i = 1; i <= N; i++) {
+            execBFS(i);
+            max = Math.max(max, count[i]);
+        }
 
         for (int i = 1; i <= N; i++) {
-            BFS(i);
-        }
-        for (int i = 1; i <= N; i++) {
-            if (max == cnt[i]) sb.append(i).append(" ");
+            if (max == count[i]) {
+                sb.append(i).append(" ");
+            }
         }
         System.out.println(sb);
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        Main main = new Main();
+        main.solution();
     }
 }
