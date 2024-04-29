2876cd4a959733a076ee85952936e5f66d028027
DDing77 <devseo2021@gmail.com>
2024-04-29 18:35:25 +0900

    BOJ_11265

---
+++ 10001~12000/11265/BOJ_11265/src/Main.java
@@ -0,0 +1,61 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static final StringBuilder sb = new StringBuilder();
+    public static StringTokenizer st;
+
+    private int N;
+    private int M;
+    private int[][] board;
+
+    private void solution() throws IOException {
+
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        board = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                board[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int k = 0; k < N; k++) {
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    board[i][j] = Math.min(board[i][j], board[i][k] + board[k][j]);
+                }
+            }
+        }
+
+        while (M-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            int from = Integer.parseInt(st.nextToken()) - 1;
+            int to = Integer.parseInt(st.nextToken()) - 1;
+            int time = Integer.parseInt(st.nextToken());
+
+            if (board[from][to] <= time) {
+                sb.append("Enjoy other party");
+            } else {
+                sb.append("Stay here");
+            }
+            sb.append("\n");
+        }
+
+        System.out.print(sb);
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        Main main = new Main();
+        main.solution();
+    }
+}
