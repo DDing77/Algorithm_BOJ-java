168a3dda7cf2d0bb39efadd63a647c74f28ee029
DDing77 <devseo2021@gmail.com>
2024-05-07 14:21:36 +0900

    BOJ_30018

---
+++ 30001~32000/30018/BOJ_30018/src/Main.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static final StringBuilder sb = new StringBuilder();
+    public static StringTokenizer st;
+
+    private int N;
+    private int[] a;
+    private int[] b;
+    private int res;
+
+    private void solution() throws IOException {
+
+        N = Integer.parseInt(br.readLine());
+        a = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            a[i] = Integer.parseInt(st.nextToken());
+        }
+
+        b = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            b[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < N; i++) {
+            if (a[i] - b[i] < 0) {
+                res += Math.abs(a[i] - b[i]);
+            }
+        }
+
+        System.out.println(res);
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        Main main = new Main();
+        main.solution();
+    }
+}
