88e931e020cbfbba89580ac71ce4162e74db5fc3
DDing77 <devseo2021@gmail.com>
2024-05-07 14:43:22 +0900

    BOJ_26145

---
+++ 26001~28000/26145/BOJ_26145/src/Main.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static final StringBuilder sb = new StringBuilder();
+    public static StringTokenizer st;
+
+    private int N;
+    private int M;
+    private int[] cost;
+
+    private void solution() throws IOException {
+
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        cost = new int[N + M];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            cost[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N + M; j++) {
+                int value = Integer.parseInt(st.nextToken());
+                cost[i] -= value;
+                cost[j] += value;
+            }
+        }
+
+        for (int value : cost) {
+            sb.append(value).append(" ");
+        }
+        System.out.print(sb);
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        Main main = new Main();
+        main.solution();
+    }
+}
