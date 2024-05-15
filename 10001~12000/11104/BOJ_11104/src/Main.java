52277a82ac57cb008df00ee218ca571d3d1602ff
DDing77 <devseo2021@gmail.com>
2024-05-15 17:01:24 +0900

    BOJ_11104

---
+++ 10001~12000/11104/BOJ_11104/src/Main.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static final StringBuilder sb = new StringBuilder();
+
+    private int N;
+
+    private void solution() throws IOException {
+
+        N = Integer.parseInt(br.readLine());
+        while (N-- > 0) {
+            String bit = br.readLine();
+            sb.append(Integer.parseInt(bit, 2)).append("\n");
+        }
+        System.out.print(sb);
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        Main main = new Main();
+        main.solution();
+    }
+}
