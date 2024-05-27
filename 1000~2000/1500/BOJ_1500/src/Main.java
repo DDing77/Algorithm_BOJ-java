2c406b4c3274407bdd7ae800d5cf77672a17aaf2
DDing77 <devseo2021@gmail.com>
2024-05-27 19:55:47 +0900

    BOJ_1500

---
+++ 1000~2000/1500/BOJ_1500/src/Main.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static final StringBuilder sb = new StringBuilder();
+    public static StringTokenizer st;
+
+    private int N;
+    private int K;
+    private long res;
+
+    private void solution() throws IOException {
+
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        int div = N / K;
+        int mod = N % K;
+
+        res = 1L;
+        for (int i = 0; i < K - mod; i++) {
+            res *= div;
+        }
+        for (int i = 0; i < mod; i++) {
+            res *= (div + 1);
+        }
+        System.out.println(res);
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        Main main = new Main();
+        main.solution();
+    }
+}
