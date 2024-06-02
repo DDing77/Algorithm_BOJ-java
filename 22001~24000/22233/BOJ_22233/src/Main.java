1d1e8abe1ecbecf4bb8e886cfb80c9275bf778d7
DDing77 <devseo2021@gmail.com>
2024-06-02 17:35:25 +0900

    BOJ_22233

---
+++ 22001~24000/22233/BOJ_22233/src/Main.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static final StringBuilder sb = new StringBuilder();
+    public static StringTokenizer st;
+
+    private int N;
+    private int M;
+    private HashSet<String> keywords;
+
+    private void solution() throws IOException {
+
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        keywords = new HashSet<>();
+        for (int i = 0; i < N; i++) {
+            keywords.add(br.readLine());
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine(), ",");
+            while (st.hasMoreTokens()) {
+                String keyword = st.nextToken();
+                if (keywords.contains(keyword)) {
+                    keywords.remove(keyword);
+                }
+            }
+            sb.append(keywords.size()).append("\n");
+        }
+        System.out.print(sb);
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        Main main = new Main();
+        main.solution();
+    }
+}
