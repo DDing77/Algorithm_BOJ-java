c664965d8abf919efbb33da0528da4897966a51f
DDing77 <devseo2021@gmail.com>
2024-08-07 21:15:11 +0900

    BOJ_16132

---
+++ 16001~18000/16132/BOJ_16132/src/Main.java
@@ -0,0 +1,57 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+
+    public static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static final StringBuilder sb = new StringBuilder();
+
+    private int N;
+    private long[][] dp;
+
+    private long getResult(int remainder, int cur) {
+        if (remainder < 0 || cur < 0) {
+            return 0;
+        }
+
+        if (dp[remainder][cur] != -1) {
+            return dp[remainder][cur];
+        }
+
+        if (remainder == 0 && cur == 0) {
+            return 1;
+        }
+
+        return dp[remainder][cur] = getResult(remainder - cur, cur - 1)
+                + getResult(remainder, cur - 1);
+    }
+
+    private void solution() throws IOException {
+
+        N = Integer.parseInt(br.readLine());
+
+        int length = N * (N + 1) / 2;
+
+        if (length % 2 == 1) {
+            System.out.println(0);
+            return;
+        }
+
+        dp = new long[length / 2 + 1][N + 1];
+        for (int i = 0; i < dp.length; i++) {
+            Arrays.fill(dp[i], -1);
+        }
+
+        getResult(length / 2, N);
+
+        System.out.println(dp[length / 2][N] / 2);
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        Main main = new Main();
+        main.solution();
+    }
+}
