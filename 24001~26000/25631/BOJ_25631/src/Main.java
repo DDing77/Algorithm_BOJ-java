ec406564445bb6d5aa74828307c09aa12c833e0e
DDing77 <devseo2021@gmail.com>
2024-10-02 22:43:24 +0900

    BOJ_25631

---
+++ 24001~26000/25631/BOJ_25631/src/Main.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static final StringBuilder sb = new StringBuilder();
+    public static StringTokenizer st;
+
+    private int N;
+    private Map<Integer, Integer> map;
+    private int res;
+
+    private void solution() throws IOException {
+
+        N = Integer.parseInt(br.readLine());
+
+        map = new HashMap<>();
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            int key = Integer.parseInt(st.nextToken());
+            map.put(key, map.getOrDefault(key, 0) + 1);
+        }
+
+        for (int value : map.values()) {
+            res = Math.max(res, value);
+        }
+
+        System.out.println(res);
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        Main main = new Main();
+        main.solution();
+    }
+}
