84ff8b2669930f91ab5a4829962bb9e024ab6f3b
DDing77 <devseo2021@gmail.com>
2024-10-23 11:32:48 +0900

    BOJ_14950

---
+++ 14001~16000/14950/BOJ_14950/src/Main.java
@@ -0,0 +1,105 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static final StringBuilder sb = new StringBuilder();
+    public static StringTokenizer st;
+
+    private int N;
+    private int M;
+    private int t;
+
+    private Edge[] edges;
+    private int[] parents;
+    private int res;
+
+    private int find(int x) {
+
+        if (parents[x] < 0) {
+            return x;
+        }
+        return parents[x] = find(parents[x]);
+    }
+
+    private boolean union(int x, int y) {
+
+        x = find(x);
+        y = find(y);
+
+        if (x == y) {
+            return false;
+        }
+
+        if (x <= y) {
+            parents[x] += parents[y];
+            parents[y] = x;
+        } else {
+            parents[y] += parents[x];
+            parents[x] = y;
+        }
+
+        return true;
+    }
+
+    private void solution() throws IOException {
+
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        t = Integer.parseInt(st.nextToken());
+
+        parents = new int[N + 1];
+        Arrays.fill(parents, -1);
+
+        edges = new Edge[M];
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+            int weight = Integer.parseInt(st.nextToken());
+            edges[i] = new Edge(from, to, weight);
+        }
+
+        Arrays.sort(edges);
+
+        int idx = 0;
+        int cnt = 0;
+        while (parents[1] != -N) {
+            if (union(edges[idx].node1, edges[idx].node2)) {
+                res += edges[idx].weight + t * cnt++;
+            }
+            idx++;
+        }
+
+        System.out.println(res);
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        Main main = new Main();
+        main.solution();
+    }
+
+    static class Edge implements Comparable<Edge> {
+
+        int node1;
+        int node2;
+        int weight;
+
+        public Edge(int node1, int node2, int weight) {
+            this.node1 = node1;
+            this.node2 = node2;
+            this.weight = weight;
+        }
+
+        @Override
+        public int compareTo(Edge o) {
+            return this.weight - o.weight;
+        }
+    }
+}
