bbba1edf5313d694fab070597dfea041e12de8d7
DDing77 <devseo2021@gmail.com>
2025-05-08 18:43:22 +0900

    BOJ_31831

---
+++ 30001~32000/31831/BOJ_31831/src/Main.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static final StringBuilder sb = new StringBuilder();
+    public static StringTokenizer st;
+
+    private int N;
+    private int M;
+    private int res;
+
+    private void solution() throws IOException {
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        int sum = 0;
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            sum = Math.max(0, sum + Integer.parseInt(st.nextToken()));
+            if (sum >= M) {
+                res++;
+            }
+        }
+
+        System.out.println(res);
+    }
+
+    public static void main(String[] args) throws IOException {
+        Main main = new Main();
+        main.solution();
+    }
+}
