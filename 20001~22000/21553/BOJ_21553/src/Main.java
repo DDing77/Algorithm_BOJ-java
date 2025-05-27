84808a0d169a39e4278a55429efb940174f41b6a
DDing77 <devseo2021@gmail.com>
2025-05-27 22:46:37 +0900

    BOJ_21553

---
+++ 20001~22000/21553/BOJ_21553/src/Main.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static final StringBuilder sb = new StringBuilder();
+    public static StringTokenizer st;
+
+    private void solution() throws IOException {
+        String A = br.readLine();
+        String P = br.readLine();
+        System.out.println(P);
+    }
+
+    public static void main(String[] args) throws IOException {
+        Main main = new Main();
+        main.solution();
+    }
+}
