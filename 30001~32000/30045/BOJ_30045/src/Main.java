82775cb2a34e42fa9c689c96440929b587a58082
DDing77 <devseo2021@gmail.com>
2026-02-16 13:30:39 +0900

    BOJ_30045

---
+++ 30001~32000/30045/BOJ_30045/src/Main.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static final StringBuilder sb = new StringBuilder();
+
+    private int N;
+    private int res;
+
+    private void solution() throws IOException {
+        N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N; i++) {
+            String input = br.readLine();
+            if (input.contains("01") || input.contains("OI")) {
+                res++;
+            }
+        }
+        System.out.println(res);
+    }
+
+    public static void main(String[] args) throws IOException {
+        Main main = new Main();
+        main.solution();
+    }
+}
