e7aabef07cb136fe44b48fcc350d19b48841bda0
DDing77 <devseo2021@gmail.com>
2026-02-20 13:52:42 +0900

    BOJ_1863

---
+++ 1000~2000/1863/BOJ_1863/src/Main.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static final StringBuilder sb = new StringBuilder();
+    public static StringTokenizer st;
+
+    private int N;
+    private Stack<Integer> stack = new Stack<>();
+    private int res;
+
+    private void solution() throws IOException {
+        N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            while (!stack.isEmpty() && stack.peek() > y) {
+                stack.pop();
+            }
+
+            if (y == 0) {
+                continue;
+            }
+
+            if (stack.isEmpty() || stack.peek() < y) {
+                stack.push(y);
+                res++;
+            }
+        }
+
+        System.out.println(res);
+    }
+
+    public static void main(String[] args) throws IOException {
+        Main main = new Main();
+        main.solution();
+    }
+}
