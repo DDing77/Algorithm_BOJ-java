0a52d190225cc03ee7d4a5bf012372fdb003f9f9
DDing77 <devseo2021@gmail.com>
2026-03-07 13:54:32 +0900

    BOJ_3765

---
+++ 26001~28000/26575/BOJ_3765/src/Main.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static final StringBuilder sb = new StringBuilder();
+    public static StringTokenizer st;
+
+    private void solution() throws IOException {
+        String line;
+
+        while ((line = br.readLine()) != null) {
+            sb.append(line).append("\n");
+        }
+
+        System.out.print(sb);
+    }
+
+    public static void main(String[] args) throws IOException {
+        Main main = new Main();
+        main.solution();
+    }
+}
