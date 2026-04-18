e5e1ee3d9f9a50440a416d4efe7fb8b85c3720b6
DDing77 <devseo2021@gmail.com>
2026-04-18 19:30:25 +0900

    BOJ_31746

---
+++ 30001~32000/31746/BOJ_31746/src/Main.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static final StringBuilder sb = new StringBuilder();
+
+    private int N;
+
+    private void solution() throws IOException {
+        N = Integer.parseInt(br.readLine());
+
+        if ((N & 1) == 1) {
+            System.out.println("evoLmoCicS");
+        } else {
+            System.out.println("SciComLove");
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        Main main = new Main();
+        main.solution();
+    }
+}
